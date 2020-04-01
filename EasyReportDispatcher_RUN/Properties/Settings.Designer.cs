d3a4f5d314bbd7cf3adb4b0ec5f7ea7254d890d3
simonep@fastwebnet.it <simonep@fastwebnet.it>
2020-04-02 01:53:50 +0200

    parametri base task

--- EasyReportDispatcher_RUN/Properties/Settings.Designer.cs
+++ EasyReportDispatcher_RUN/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace EasyReportDispatcher_RUN.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,60 @@ namespace EasyReportDispatcher_RUN.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string TaskLogDir {
+            get {
+                return ((string)(this["TaskLogDir"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string TaskLogMailFrom {
+            get {
+                return ((string)(this["TaskLogMailFrom"]));
+            }
+            set {
+                this["TaskLogMailFrom"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string TaskLogMailTO {
+            get {
+                return ((string)(this["TaskLogMailTO"]));
+            }
+            set {
+                this["TaskLogMailTO"] = value;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public int TaskLogSmtpConfigId {
+            get {
+                return ((int)(this["TaskLogSmtpConfigId"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("90")]
+        public int TaskLogKeepNum {
+            get {
+                return ((int)(this["TaskLogKeepNum"]));
+            }
+            set {
+                this["TaskLogKeepNum"] = value;
             }
         }
     }
