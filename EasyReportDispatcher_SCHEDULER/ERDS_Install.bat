7fd410ff9e0abc9d8feca556b65b59dbc075208a
Simone Pelaia <spelaia@postewelfareservizi.it>
2020-07-27 09:24:45 +0200

    Aggiunti batch di installazione, disinstallazione e lancio console

---
+++ EasyReportDispatcher_SCHEDULER/ERDS_Install.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+
+echo INSTALLAZIONE SERVIZIO ERD SCHEDULER (ERD-Scheduler)
+
+EasyReportDispatcher_SCHEDULER.exe /install
+
+net start ERD-Scheduler
+
+pause
---
+++ EasyReportDispatcher_SCHEDULER/ERDS_Uninstall.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+
+echo DISINSTALLAZIONE SERVIZIO ERD SCHEDULER (ERD-Scheduler)
+
+net stop ERD-Scheduler
+
+EasyReportDispatcher_SCHEDULER.exe /uninstall
+
+pause
